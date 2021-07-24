4223d90c0e307a03381239e4bf68e1a964464338
유도건 <dgyoo@nextree.io>
2021-07-24 13:25:23 +0900

    20200724

--- src/com/company/easy/PalindromeNumber.java
+++ src/com/company/easy/PalindromeNumber.java
@@ -1,2 +1,27 @@
-package com.company.easy;public class PalindromeNumber {
+package com.company.easy;
+
+public class PalindromeNumber {
+    //
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(12321));
+    }
+    public static boolean isPalindrome(int x) {
+        //
+        if (x < 0) {
+            return false;
+        }
+        int result = 0;
+        int num = x;
+
+        while(true) {
+            //
+            result = result * 10 + num % 10;
+            num /= 10 ;
+
+            if (num==0) {
+                return result == x;
+            }
+        }
+
+    }
 }
--- src/com/company/easy/RomanToInteger.java
+++ src/com/company/easy/RomanToInteger.java
@@ -1,2 +1,48 @@
-package com.company.easy;public class RomanToInteger {
+package com.company.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    //
+    public static void main(String[] args) {
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+
+    public static int romanToInt(String s) {
+        //
+        Map<Character,Integer> roman = new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+
+        char[] foo = s.toCharArray();
+        int result = 0;
+
+        for (int i = 0; i < foo.length; i++) {
+            //
+            int num = roman.get(foo[i]);
+            int inx = 0;
+
+            if(foo.length > i +1) {
+                //
+                inx = roman.get(foo[i+1]);
+            }
+
+            if (inx>num) {
+                i += 1;
+                result = result + inx-num;
+            } else {
+                result += num;
+            }
+
+        }
+
+        return result;
+    }
 }
