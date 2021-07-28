46d7b1c6d36a28f5f619a1a9775808c97b7437c7
유도건 <dgyoo@nextree.io>
2021-07-28 09:46:22 +0900

    20210728

--- src/com/company/easy/BuildArrayFromPermutation.java
+++ src/com/company/easy/BuildArrayFromPermutation.java
@@ -1,2 +1,19 @@
-package com.company.easy;public class BuildArrayFromPermutation {
+package com.company.easy;
+
+public class BuildArrayFromPermutation {
+
+    public static void main(String[] args) {
+        //
+        System.out.println(buildArray(new int[]{5,0,1,2,3,4}));
+    }
+    public static int[] buildArray(int[] nums) {
+        //
+        int[] result = new int[nums.length];
+
+        for (int j = 0; j < nums.length; j++) {
+            result[j] = nums[nums[j]];
+        }
+
+        return result;
+    }
 }
--- src/com/company/easy/ConcatenationOfArray.java
+++ src/com/company/easy/ConcatenationOfArray.java
@@ -1,2 +1,21 @@
-package com.company.easy;public class ConcatenationOfArray {
+package com.company.easy;
+
+public class ConcatenationOfArray {
+
+    public static void main(String[] args) {
+        getConcatenation(new int[]{1,2,3});
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        //
+        int[] result = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            //
+//            int foo = i % nums.length;
+            result[i] = nums[i];
+            result[i+nums.length] = nums[i];
+        }
+        return result;
+    }
 }
