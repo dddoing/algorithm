c029193dfdda488f1f849698f1c0df5c0b61a158
유도건 <dgyoo@nextree.io>
2021-09-16 14:02:02 +0900

    test

---
+++ src/com/company/easy/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+package com.company.easy;
+
+public class LongestCommonPrefix {
+    //
+    public static void main(String[] args) {
+        //
+        String[] words = {"flower", "flow", "flight"};
+        longestCommonPrefix(words);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        //
+        String result = "";
+        System.out.println(System.currentTimeMillis());
+
+        return result;
+    }
+}
