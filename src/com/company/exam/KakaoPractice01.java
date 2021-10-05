985bff04cf509aa7ca70f9526d3369d1bdad8c34
유도건 <dgyoo@nextree.io>
2021-10-05 16:44:20 +0900

    20211005

--- src/com/company/exam/KakaoPractice01.java
+++ src/com/company/exam/KakaoPractice01.java
@@ -1,5 +1,7 @@
 package com.company.exam;
 
+import java.util.regex.Pattern;
+
 public class KakaoPractice01 {
     //
     /**
@@ -21,31 +23,41 @@ public class KakaoPractice01 {
     public static void main(String[] args) {
         //
         System.out.println(func("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(func("z-+.^."));
+        System.out.println(func("=.="));
+        System.out.println(func("123_.def"));
+        System.out.println(func("abcdefghijklmn.p"));
     }
 
     public static String func(String text) {
         //
-
+        String value = text;
         //1. toLowerCase()
-        return "";
-    }
+        value = value.toLowerCase();
 
-    public static String Reg(String text) {
-        //
+        //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+        value = value.replaceAll("[^0-9a-z-_.]","");
 
-        //2. Reg
-        //3. Reg
-        //4. Reg
+        //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+        value = value.replaceAll("[.]{2,}",".");
 
-        return "";
-    }
+        //4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        value = value.replaceAll("^[.]|[.]$","");
 
-    public static String sub(String text) {
-        //
+        //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        value = value.length() == 0 ? "a" : value;
 
-        //5.
-        //6.
-        return "";
+        //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+        //        만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+        value = value.length() >= 16 ? value.substring(0,15).replaceAll("[.]$","") : value;
+        //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+        if (value.length() <= 2) {
+            while (value.length() < 3) {
+                value += value.charAt(value.length()-1);
+            }
+        }
+
+        return value;
     }
 
 }
